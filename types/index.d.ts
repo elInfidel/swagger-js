1bc2e4f1316dc4eeb2b616eafa516bb4f5c0e629
Liam Parker <parker.liam5@gmail.com>
2021-03-18 19:58:42 +1100

    Added changes

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,17 +1,28 @@
-interface ISwaggerClient {
-  execute: (operationId: string | ExecuteOptions, opts?: ExecuteOptions) => void;
-  http: Http,
-  makeHttp = makeHttp.bind(null, Swagger.http),
-  resolve,
-  resolveSubtree,
-  execute,
-  serializeRes,
-  serializeHeaders,
-  clearCache,
-  makeApisTagOperation,
-  buildRequest,
-  helpers,
-  getBaseUrl,
+type ClientRequest = Request & RequestExtensions;
+type ClientResponse = Response; // Any additional params?
+
+/**
+ * Additional values attached to the request object but not part of the official Request API.
+ */
+interface RequestExtensions {
+  /**
+   * When provided, the query parameters contained are serialized and appended to Request.url property as query string.
+   */
+  query: object | null;
+  /**
+   * Signals when the Request was constructed implicitly by SwaggerClient.
+   */
+  loadSpec: boolean | undefined;
+  /**
+   * When set, it intercepts the Request object before the actual HTTP request
+   * is made and after the query serialization kicked in. This means that
+   * intercepted Request object will never contain query property. All other properties
+   * will be present though. Request interceptor can be defined as synchronous (transformer) or
+   * asynchronous function (allows other async operations inside).
+   */
+  requestInterceptor: (request: ClientRequest) => ClientRequest;
+  responseInterceptor: (response: ClientResponse) => ClientResponse;
+  userFetch: (url: string, request: Request) => Promise<Response>;
 }
 
 interface BearerAuthorization {
@@ -40,28 +51,96 @@ interface Securities {
   oAuth2?: OAuth2Authorization;
 }
 
+/**
+ * Parameters used to configure an operation on a swagger specification.
+ */
 interface ExecuteOptions<T> {
-  spec: object; // TODO: how to type this?
+  /**
+   * OpenAPI definition represented as POJO.
+   */
+  spec?: any; // TODO: how to type this?
+  /**
+   * Unique string used to identify an operation. If not provided, pathName + method must be used instead.
+   */
   operationId?: string;
+  /**
+   * OpenAPI defines a unique operation as a combination
+   * of a path and an HTTP method. If operationId is not
+   * provided, this property must be set.
+   */
   pathName?: string;
+  /**
+   * OpenAPI defines a unique operation as a combination
+   * of a path and an HTTP method. If operationId is not provided,
+   * this property must be set.
+   */
   method?: 'GET' | 'HEAD' | 'POST' | 'PUT' | 'DELETE' | 'CONNECT' | 'OPTIONS' | 'TRACE' | 'PATCH';
-  parameters: T;
-  securities: Securities;
-  requestInterceptor: (request: Request) => Request;
-  responseInterceptor: (response: Response) => Response;
-  requestContentType: string;
-  responseContentType: string;
-  attachContentTypeForEmptyPayLoad: boolean = false;
-  http: Http;
-  userFetch: (url: string, request: Request) => Response;
+  /**
+   * Parameters object, eg: { q: 'search string' }. Parameters not defined in spec will be ignored.
+   */
+  parameters?: T;
+  /**
+   * Maps security schemes to a request. Securities not defined in spec will be ignored.
+   */
+  securities?: Securities;
+  /**
+   * Either synchronous or asynchronous function transformer that accepts Request and should return Request.
+   */
+  requestInterceptor?: (request: Request) => Request;
+  /**
+   * Either synchronous or asynchronous function transformer that accepts Response and should return Response.
+   */
+  responseInterceptor?: (response: Response) => Response;
+  /**
+   * Sets appropriate media type for request body, e.g. application/json.
+   * If supplied media type is not defined for the request body, this property is ignored.
+   */
+  requestContentType?: string;
+  /**
+   * Expected appropriate media type response, e.g. application/json.
+   * Creates an Accept header in Request object.
+   */
+  responseContentType?: string;
+  /**
+   * Attaches a Content-Type header to a Request even when no payload was provided for the Request.
+   */
+  attachContentTypeForEmptyPayLoad?: boolean;
+  /**
+   * A function with an interface compatible with HTTP Client.
+   */
+  http?: any; // TODO: How to type this?
+  /**
+   * Custom asynchronous fetch function that accepts two arguments: the url and the Request object
+   * and must return a Response object. More info in HTTP Client documentation.
+   */
+  userFetch?: (url: string, request: Request) => Response;
 }
 
-function Http()
-
-class SwaggerClient implements ISwaggerClient {
-  constructor(url: string | ClientOptions, opts?: ClientOptions);
+/**
+ * options used for construction of a swagger client object.
+ */
+interface ClientOptions {
+  // TODO
 }
 
-function SwaggerFunction(url: string | ClientOptions, opts?: ClientOptions): Promise<ISwaggerClient>;
+declare interface SwaggerClient {
+  /**
+   * TryItOut Executor is an OpenAPI specific HTTP client for OAS operations.
+   * It maps an OAS operation and values into an HTTP request.
+   */
+  execute: <T>(operationId: string | ExecuteOptions<T>, opts?: ExecuteOptions<T>) => void;
+  /**
+   * The HTTP Client exposes a Fetch-like interface.
+   * It extends Fetch API to support request and response interceptors and performs response & header serialization.
+   */
+  http: (url: string | ClientRequest, request?: ClientRequest) => Promise<ClientResponse>;
+}
 
-export default SwaggerClient;
+interface SwaggerClientConstructor {
+  new (url: string | ClientOptions, opts?: ClientOptions): SwaggerClient;
+  (url: string | ClientOptions, opts?: ClientOptions): SwaggerClient;
+}
+
+declare const Client: SwaggerClientConstructor;
+
+export default Client;
